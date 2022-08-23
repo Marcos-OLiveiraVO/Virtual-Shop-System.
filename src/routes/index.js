90c1324341264e26125c500c16263abe8f735124
Marcos Oliveira <marcosoliveira.rd@gmail.com>
2022-08-22 23:16:35 -0400

    index.js userRouter uploaded

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.use(express.json());
 router.use("/products", productsRoute);
-router.uses("users", usersRoute);
+router.use("users", usersRoute);
 
 router.get("/", (req, res) => {
   res.send("Hello World");
