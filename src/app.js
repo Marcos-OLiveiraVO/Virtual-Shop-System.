b6856d256de8f25d91efa8cc657f0ead16a7a212
Marcos Oliveira <marcosoliveira.rd@gmail.com>
2022-08-27 23:15:24 -0400

    acess controll list feature

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
 import router from "./routes";
 import database from "./database";
+import acl from "express-acl";
 
 const app = express();
 
+acl.config({
+  baseUrl: "/",
+  path: "config",
+});
+
 const configureExpress = () => {
   app.use(express.json());
+  app.use(acl.authorize.unless({ path: ["/users/authenticate"] }));
   app.use("/", router);
   app.database = database;
 
