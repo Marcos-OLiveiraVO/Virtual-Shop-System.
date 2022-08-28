d9791acb86a72754c3881c97983f3d054fb2074e
Marcos Oliveira <marcosoliveira.rd@gmail.com>
2022-08-27 22:52:07 -0400

    User cryptography bcript feature

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,26 @@
 import mongoose from "mongoose";
+import Util from "util";
+import bcript from "bcrypt";
+
+const hashAsync = Util.promisify(bcript.hash);
 
 const schema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
   role: String,
+});
+
+schema.pre("save", async function (next) {
+  if (!this.password || !this.isModified("password")) {
+    return next();
+  }
+  try {
+    const hashedPassword = await hashAsync(this.password, 10);
+    this.password = hashedPassword;
+  } catch (err) {
+    next(err);
+  }
 });
 
 schema.set("toJSON", {
